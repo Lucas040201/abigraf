a7e4e91999c4671c4cf0e1192ac446ba453baf30
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2021-12-14 06:59:29 -0300

    css e diretorio

--- wp-content/themes/abigraf/front-page.php
+++ wp-content/themes/abigraf/front-page.php
@@ -6,32 +6,32 @@ get_header();
 
     <section class="wrapper main-slider">
         <a href="" target="_blank">
-            <img class="slider-item" src="/assets/images/ex/NR12_AN 1.png" alt="">
+            <img class="slider-item" src="<?php echo get_template_directory_uri(); ?>/assets/images/ex/NR12_AN 1.png" alt="">
         </a>
         <a href="" target="_blank">
-            <img class="slider-item" src="/assets/images/ex/NR12_AN 1.png" alt="">
+            <img class="slider-item" src="<?php echo get_template_directory_uri(); ?>/assets/images/ex/NR12_AN 1.png" alt="">
         </a>
     </section>
     <section class="wrapper regionais-home">
         <div class="regionais-home__text">
             <h3 class="titulo">Regionais Abigraf</h3>
             <p>A ABIGRAF possui estrutura de representação em 21 Estados e no Distrito Federal, atuando de forma coordenada em busca de resultados concretos e de melhorias para a Indústria Gráfica Brasileira.</p>
-            <span><img src="/assets/images/icons/mapa-gps.svg" alt="">Navegue no mapa e saiba mais</span>
+            <span><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/mapa-gps.svg" alt="">Navegue no mapa e saiba mais</span>
         </div>
         <a href="">
             <map name="">
-                <img src="/assets/images/ex/map-sample.png" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ex/map-sample.png" alt="">
             </map>
         </a>
     </section>
     <section class="wrapper">
         <div class="linknews-home">
             <div class="aside">
-                <a style="background-image: url(/assets/images/ex/galeria.png);" href="" class="aside__galeria">
+                <a style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/ex/galeria.png);" href="" class="aside__galeria">
                     <span>Galeria de Fotos</span>
                     <p>Confira as fotos dos eventos da Abigraf. </p>
                 </a>
-                <a style="background-image: url(/assets/images/ex/guia.png);" href="#" class="aside__guia">
+                <a style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/ex/guia.png);" href="#" class="aside__guia">
                     <span>Guia Produção Sustentável</span>
                     <p>Faça o download do arquivo aqui e saiba mais</p>
                 </a>
@@ -46,7 +46,7 @@ get_header();
                 <p>A TV ABIGRAF mostra todas ações das nossas entidades como depoimentos, opiniões, eventos, treinamentos e palestras. </p>
                 <a href="" class="button">Assista</a>
             </article>
-            <img src="/assets/images/ex/box-tv-abigraf 1.png" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ex/box-tv-abigraf 1.png" alt="">
         </div>
     </section>
     <section class="faixa-amarela faixa-amarela--agenda">
@@ -55,7 +55,7 @@ get_header();
             <div class="slider-agenda">
                 <div class="slider-agenda__card">
                     <article>
-                        <span class="slider-agenda__date"><img src="/assets/images/icons/calendar.svg" alt="">13/07</span>
+                        <span class="slider-agenda__date"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/calendar.svg" alt="">13/07</span>
                         <h3>Abigraf Nacional e ABTG</h3>
                         <p>Programa Capacite-se Palestra - Os Fundamentos do Gerenciamento de Cores na prática</p>
                         <a href="">Saiba mais</a>
@@ -64,7 +64,7 @@ get_header();
 
                 <div class="slider-agenda__card">
                     <article>
-                        <span class="slider-agenda__date"><img src="/assets/images/icons/calendar.svg" alt="">13/07</span>
+                        <span class="slider-agenda__date"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/calendar.svg" alt="">13/07</span>
                         <h3>Abigraf Nacional e ABTG</h3>
                         <p>Programa Capacite-se Palestra - Os Fundamentos do Gerenciamento de Cores na prática</p>
                         <a href="">Saiba mais</a>
@@ -73,7 +73,7 @@ get_header();
 
                 <div class="slider-agenda__card">
                     <article>
-                        <span class="slider-agenda__date"><img src="/assets/images/icons/calendar.svg" alt="">13/07</span>
+                        <span class="slider-agenda__date"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/calendar.svg" alt="">13/07</span>
                         <h3>Abigraf Nacional e ABTG</h3>
                         <p>Programa Capacite-se Palestra - Os Fundamentos do Gerenciamento de Cores na prática</p>
                         <a href="">Saiba mais</a>
@@ -85,38 +85,38 @@ get_header();
     <section class="wrapper cards">
         <div class="cards__item">
             <div class="cards__icon">
-                <img src="/assets/images/icons/juridico.svg" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/juridico.svg" alt="">
                 <span>Jurídico</span>
             </div>
-            <article style="background-image: url(/assets/images/ex/juridico.png);">
+            <article style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/ex/juridico.png);">
                 <p>Assessoria profissional consultiva personalizada nas áreas trabalhista, tributária e previdenciária. </p>
                 <a href="" class="button">Conheça</a>
             </article>
         </div>
         <div class="cards__item">
             <div class="cards__icon">
-                <img src="/assets/images/icons/dados-economicos.svg" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/dados-economicos.svg" alt="">
                 <span>Dados Econômicos</span>
             </div>
-            <article style="background-image: url(/assets/images/ex/dados-economicos.png);">
+            <article style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/ex/dados-economicos.png);">
                 <p>Conteúdo exclusivo aos associados: série de dados referentes ao setor como análises, negociações e política industrial.</p>
                 <a href="" class="button">Acesse</a>
             </article>
         </div>
         <div class="cards__item">
             <div class="cards__icon">
-                <img src="/assets/images/icons/associe-se.svg" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/associe-se.svg" alt="">
                 <span>Associe-se</span>
             </div>
-            <article style="background-image: url(/assets/images/ex/associe-se.png);">
+            <article style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/ex/associe-se.png);">
                 <p>Conheça nosso Guia de Benefícios e veja as vantagens de ser um dos nossos Associados.</p>
                 <a href="" class="button">Conheça</a>
             </article>
         </div>
     </section>
     <section class="wrapper dualbanners">
-        <a href=""><img src="/assets/images/ex/corona.jpg" alt=""></a>
-        <a href=""><img src="/assets/images/ex/c]onlatingraf.png" alt=""></a>
+        <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ex/corona.jpg" alt=""></a>
+        <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ex/c]onlatingraf.png" alt=""></a>
     </section>
 </main>
 
