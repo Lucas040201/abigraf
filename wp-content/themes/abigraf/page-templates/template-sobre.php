f9e7454dd8270da6cdd33b29db0c5a12a110f520
Lucas040201 <lucasdasilvamendes123456@hotmail.com>
2022-01-02 17:41:57 -0300

    titulo sobre

--- wp-content/themes/abigraf/page-templates/template-sobre.php
+++ wp-content/themes/abigraf/page-templates/template-sobre.php
@@ -8,10 +8,11 @@
 
 get_header();
 get_template_part('template-parts/components/content-aside');
+$estado = get_field('estado')['label'];
 ?>
 <main>
     <section class="wrapper sobre">
-        <h3 class="titulo"><?php echo get_the_title(); ?></h3>
+        <h3 class="titulo"><?php echo "Abigraf $estado";?></h3>
 
         <article>
             <?php the_content(); ?>
