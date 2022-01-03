9e035fadf15ab6f21a88ea90c03f44b1d725ea13
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2022-01-03 07:12:52 -0300

    galeria presidentes

---
+++ wp-content/themes/abigraf/page-templates/galeria-presidentes.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Template Name: Template Galeria Presidentes
+ * Template Post Type: page
+ * @since 1.0.0
+ */
+
+get_header('seccional');
+?>
+<main>
+    <section class="wrapper galeria-presidentes">
+    <?php
+        if( have_rows('presidentes') ):
+            while( have_rows('presidentes') ) : the_row();
+            ?>
+            <div class="galeria-presidentes__card">
+                <img src="<?php echo get_sub_field('foto'); ?>" alt="">
+                <span><?php echo get_sub_field('data'); ?></span>
+                <hr>
+                <h3><?php echo get_sub_field('nome'); ?></h3>
+                <p><?php echo get_sub_field('cargo'); ?></p>
+            </div>
+            <?php
+            endwhile;
+        endif;
+    ?>
+    </section>
+</main>
+
+<?php get_footer(); ?>
